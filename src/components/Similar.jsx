662e0c8e921b9c2fb70d45a4bc304b5c54de5b55
sultonoir <127225240+sultonoir@users.noreply.github.com>
2023-03-13 12:13:41 +0700

    basic

--- src/components/Similar.jsx
+++ src/components/Similar.jsx
@@ -74,14 +74,7 @@ const Similar = ({ id, type }) => {
                 <LazyLoadImage className="rounded-lg" src={`https://image.tmdb.org/t/p/w500/${c?.poster_path}`} alt={c.title} />
               </picture>
               <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white rounded-lg">
-                <Link
-                  to={`/${type}/${c.id}`}
-                  onContextMenu={(e) => {
-                    e.preventDefault();
-                    window.open(`/${type}/${c.id}`, "_blank", "noopener noreferrer");
-                  }}
-                  className="whitespace-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center p-2 cursor-pointer"
-                >
+                <Link to={`/${type}/${c.id}`} className="whitespace-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center p-2 cursor-pointer">
                   {c?.title || c?.name}
                 </Link>
               </div>
